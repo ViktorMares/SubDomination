c73573bab60b557b80e48176951717a2eea2dd81
VM <80492489+ViktorMares@users.noreply.github.com>
2023-11-06 18:21:47 +0200

    Update subdomination.py
    Adding subfinder & sublist3r commands to be used, for wider subdomain coverage

--- subdomination.py
+++ subdomination.py
@@ -15,21 +15,33 @@ args = parser.parse_args()
 domain = args.domain
 
 print(f'\n[+] Enumerating subdomains of {domain}\n')
-cmd = f'assetfinder {domain} | grep "{domain}" | sort | uniq '
-ps = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-output = ps.communicate()[0]
+
+# Use Sublist3r to enumerate subdomains
+sublist3r_cmd = f'sublist3r -d {domain} | sort -u'
+sublist3r_output = subprocess.check_output(sublist3r_cmd, shell=True, text=True, stderr=subprocess.DEVNULL)
+
+# Use Subfinder to enumerate subdomains
+subfinder_cmd = f'subfinder -d {domain} | sort -u'
+subfinder_output = subprocess.check_output(subfinder_cmd, shell=True, text=True, stderr=subprocess.DEVNULL)
+
+# Use Assetfinder to enumerate subdomains
+assetfinder_cmd = f'assetfinder --subs-only {domain} | sort -u'
+assetfinder_output = subprocess.check_output(assetfinder_cmd, shell=True, text=True, stderr=subprocess.DEVNULL)
+
+# Combine the results from Sublist3r, Subfinder, and Assetfinder
+combined_output = f"{sublist3r_output}{subfinder_output}{assetfinder_output}"
 
 subdomains = []
 count = 0
 
-for subd in output.decode('utf-8').split('\n'):
+for subd in combined_output.split('\n'):
     subdomains.append(subd)
     count += 1
 
 print(f'[+] Found {count} subdomain(s) for {domain}')
 
 print(f'''
-[+] Testing the connection to each subdomain 
+[+] Testing the connection to each subdomain
 [+] Listing alive subdomains only (This will take some time...)
 ''')
 
